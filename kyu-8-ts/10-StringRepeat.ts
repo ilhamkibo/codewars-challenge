de1dd4ae85104419ad8731821dcb8d996e7e7696
unknown <ilham.ilam59@gmail.com>
2026-05-13 22:15:04 +0700

    day 3 kyu 8-ts

---
+++ kyu-8-ts/10-StringRepeat.ts
@@ -0,0 +1,6 @@
+export function repeatStr(n: number, s: string): string {
+  return s.repeat(n)
+}
+
+console.log(repeatStr(2,"a"))
+console.log(repeatStr(4,"hahi"))
---
+++ kyu-8-ts/9-OppositeNumber.ts
@@ -0,0 +1,11 @@
+export class Kata {
+  static opposite(n: number) {
+    return n*(-1)
+  }
+}
+
+
+console.log(Kata.opposite(10))
+console.log(Kata.opposite(-10))
+console.log(Kata.opposite(8))
+console.log(Kata.opposite(-8))
